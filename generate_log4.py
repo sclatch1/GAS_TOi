856627175885a40378fe5af297039d6ca4dd0e15
sclatch1 <david.scalais@gmail.com>
2022-03-27 17:11:43 +0200

    generate_log4.py aangepast

--- generate_log4.py
+++ generate_log4.py
@@ -1,32 +1,30 @@
 
-output = "<html>" + "<h1>Log</h1>"
-output += "<TABLE BORDER=1>"
+class Output:
+    def __init__(self):
+        self.lijst = ["tijdstip","stack","naam","Nieuwe bestellingen","Wachtende bestellingen","wit","melk","zwart","honing","marshmallow","chili"]
+        self.tijdstip_1 = []
+        self.tijdstip_2 = []
+        self.tijdstip_3 = []
+        self.tijdstip_4 = []
+        self.tijdstip_5 = []
+        self.tijdstip_6 = []
 
-lijst = ["tijdstip","stack","naam","Nieuwe bestellingen","Wachtende bestellingen","wit","melk","zwart","honing","marshmallow","chili"]
-
-rowCounter = 0
-collumnCounter = 0
-for i in range(6):
-    eersterij = "<TR>"
-    rij = "</TR>"
-    for item in lijst:
-        if rowCounter == 0:
+    def generate_html(self):
+        output = "<html>" + "<h1>Log</h1>"
+        output += "<TABLE BORDER=1>"
+        rowCounter = 0
+        collumnCounter = 0
+        # 1ste rij van tabel wordt aangemaakt met alle elementen van self.lijst
+        eersterij = "<TR>"
+        for item in self.lijst:
             eersterij += "<TD>" + item + "</TD>"
-        elif rowCounter > 0 and collumnCounter == 0:
-            rij += "<TD>" + str(rowCounter) + "</TD>"
-        else:
-            rij += "<TD>" + "</TD>"
-        collumnCounter += 1
-    collumnCounter = 0
-    rij += "</TR>"
-    eersterij += "</TR>"
-    if rowCounter == 0:
+        eersterij += "</TR>"
         output += eersterij
-    else:
-        output += rij
-    rowCounter += 1
 
+        #tabel eindigt hier
+        output += "</table> </html>"
+        hs = open("log4.html", 'w')
+        hs.write(output)
 
-output += "</table> </html>"
-hs = open("log4.html", 'w')
-hs.write(output)
+Out = Output()
+Out.generate_html()
